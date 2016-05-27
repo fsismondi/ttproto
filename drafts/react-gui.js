f95fa6faa7b1e17edd7115ed8f56ab62873d4a8d
Tompoariniaina Andriamilanto <tompoariniaina.andriamilanto@irisa.fr>
2016-05-27 12:20:38 +0200

    feat(gui): We manage to get the option choose but still a problem with checked radio buttons

--- drafts/react-gui.js
+++ drafts/react-gui.js
@@ -1,3 +1,23 @@
+// ############### React code ###############
+
+// A renderer for the radio buttons
+var RadioButton = React.createClass({
+
+	/**
+	 * Render function for InputGroupBloc
+	 */
+	render: function() {
+		var selected = (this.props.currentAction == this.props.inputValue);
+		console.log('selected = ' + selected);
+		return (
+			<label className={ (selected) ? 'btn btn-info' : 'btn btn-info' } >
+				<input type="radio" id={this.props.inputValue} name={this.props.inputName} checked={selected} value={this.props.inputValue} onChange={this.props.switchAction} /> {this.props.inputText}
+			</label>
+		);
+	}
+});
+
+
 // A renderer for the input groups
 var InputGroupBloc = React.createClass({
 
@@ -8,17 +28,17 @@ var InputGroupBloc = React.createClass({
 
 		// If addon on right
 		if (this.props.textOnLeft) return (
-			<div class="input-group">
-				<span class="input-group-addon">{this.props.addonText}</span>
-				<input type="{this.props.inputType}" class="form-control" name="{this.props.inputName}" placeholder="{this.props.inputPlaceholder}" />
+			<div className="input-group">
+				<span className="input-group-addon">{this.props.addonText}</span>
+				<input type={this.props.inputType} className="form-control" name={this.props.inputName} placeholder={this.props.inputPlaceholder} />
 			</div>
 		);
 
 		// If addon on left
 		else return (
-			<div class="input-group">
-				<input type="{this.props.inputType}" class="form-control" name="{this.props.inputName}" placeholder="{this.props.inputPlaceholder}" />
-				<span class="input-group-addon">{this.props.addonText}</span>
+			<div className="input-group">
+				<input type={this.props.inputType} className="form-control" name={this.props.inputName} placeholder={this.props.inputPlaceholder} />
+				<span className="input-group-addon">{this.props.addonText}</span>
 			</div>
 		);
 	}
@@ -28,14 +48,18 @@ var InputGroupBloc = React.createClass({
 // The FormBloc renderer
 var FormBloc = React.createClass({
 
+	switchAction: function(optionChoosed) {
+		this.state.action = optionChoosed.currentTarget.value;
+		console.log("Action = " + optionChoosed.currentTarget.value);
+		console.log("New action = " + this.state.action);
+	},
+
 	/**
 	 * Getter of the initial state for parameters
 	 */
 	getInitialState: function() {
 		return {
-			baseUrl: 'http://127.0.0.1:2080',
-			analyseAction: '/api/v1/testcase_analyse',
-			dissectAction: '/api/v1/frames_dissect'
+			action: 'analyse'
 		};
 	},
 
@@ -43,35 +67,54 @@ var FormBloc = React.createClass({
 	 * Render function for FormBloc
 	 */
 	render: function() {
+
+		// The things that will change in the view
+		var url = 'http://127.0.0.1:2080';
+		var fileTitle = 'Pcap field to ' + this.state.action;
+		var optionsTitle = '';
+		var optionInputName = '';
+		var optionAddonText = '';
+
+		// Analyse action
+		if (this.state.action == 'analyse') {
+			url += '/api/v1/testcase_analyse';
+			optionsTitle = 'Analysis options';
+			optionInputName = 'testcase_id';
+			optionAddonText = 'Please choose a test case';
+
+		// Dissect action
+		} else {
+			url += '/api/v1/frames_dissect';
+			optionsTitle = 'Dissection options';
+			optionInputName = 'protocol_selection';
+			optionAddonText = 'Please choose a protocol';
+		}
+
 		return (
-			<form action="{this.state.baseUrl + this.state.analyseAction}" method="post" enctype="multipart/form-data">
-				<div class="col-sm-6">
-					<div class="page-header">
-						<h1>Pcap file to analyse</h1>
+			<form action={url} method="post" enctype="multipart/form-data">
+				<div className="col-sm-6">
+					<div className="page-header">
+						<h1>{fileTitle}</h1>
 					</div>
-					<InputGroupBloc inputName="pcap" inputType="file" addonText="Enter your pcap file" textOnLeft="True" inputPlaceholder="" />
+					<InputGroupBloc inputName="pcap" inputType="file" addonText="Enter your pcap file" textOnLeft={true} inputPlaceholder="" />
 				</div>
 
-				<div class="col-sm-6">
-					<div class="page-header">
-						<h1>Analyse options</h1>
+				<div className="col-sm-6">
+					<div className="page-header">
+						<h1>{optionsTitle}</h1>
 					</div>
-					<InputGroupBloc inputName="frame-number" inputType="text" addonText="Enter your pcap file" textOnLeft="Frame number" inputPlaceholder="Enter a frame number if only one wanted" />
+					<InputGroupBloc inputName="frame-number" inputType="text" addonText="Frame number" textOnLeft={true} inputPlaceholder="Enter a frame number if only one wanted" />
 
-					<div style="{{textAlign: center}}">
-						<div class="btn-group" data-toggle="buttons">
-							<label class="btn btn-info active">
-								<input type="radio" id="analyse-option" autocomplete="off" name="options" value="analyse" checked /> Analyse
-							</label>
-							<label class="btn btn-info">
-								<input type="radio" id="dissect-option" autocomplete="off" name="options" value="dissect" /> Dissect
-							</label>
+					<div style={{textAlign: 'center'}}>
+						<div className="" >
+							<RadioButton inputName="options" inputValue="analyse" inputText="Analyse" currentAction={this.state.action} switchAction={this.switchAction} />
+							<RadioButton inputName="options" inputValue="dissect" inputText="Dissect" currentAction={this.state.action} switchAction={this.switchAction} />
 						</div>
 					</div>
 				</div>
 
-				<p style="{{textAlign: center}}">
-					<input type="submit" value="Execute" class="btn btn-success centered-block" />
+				<p style={{textAlign: 'center'}}>
+					<input type="submit" value="Execute" className="btn btn-success centered-block" />
 				</p>
 			</form>
 		);
@@ -87,7 +130,7 @@ var PcapForm = React.createClass({
 	 */
 	render: function() {
 		return (
-			<div class="row">
+			<div className="row">
 				<FormBloc />
 			</div>
 		);
