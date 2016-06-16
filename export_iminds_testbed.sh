e148a2566f06ebb99185c802b762467386ddeff4
Federico Sismondi <federicosismondi@gmail.com>
2016-06-16 11:11:16 +0200

    feat export script generic for any GIT prject, also does compression and decompression on server side

--- export_iminds_testbed.sh
+++ export_iminds_testbed.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-git archive --format=tar --prefix=ttproto/ master | gzip >ttproto.tar.gz
-scp -r -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey ttproto.tar.gz fsismond@193.190.127.249:/groups/wall2-ilabt-iminds-be/f-interop-coap/ttproto.tar.gz
+PROJ=$(basename `git rev-parse --show-toplevel`)
+git archive --format=tar --prefix="$PROJ/" master | gzip >"$PROJ.tar.gz"
+scp -r -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey "$PROJ.tar.gz" fsismond@193.190.127.249:/groups/wall2-ilabt-iminds-be/f-interop-coap/
+ssh -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey fsismond@coord.iter01.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be "cd /groups/wall2-ilabt-iminds-be/f-interop-coap; tar -zxvf "$PROJ.tar.gz""
