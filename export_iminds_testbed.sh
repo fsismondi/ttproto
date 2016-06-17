dc4f4e94da375605e80391d51ae3a27d7b3d8318
Federico Sismondi <federicosismondi@gmail.com>
2016-06-17 14:29:49 +0200

    fix(export/deploy)change hostname by host ip, sometimes hostname cannot be resolved

--- export_iminds_testbed.sh
+++ export_iminds_testbed.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 PROJ=$(basename `git rev-parse --show-toplevel`)
+HOST_IP=193.190.127.249
 git archive --format=tar --prefix="$PROJ/" master | gzip >"$PROJ.tar.gz"
-scp -r -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey "$PROJ.tar.gz" fsismond@193.190.127.249:/groups/wall2-ilabt-iminds-be/f-interop-coap/
-ssh -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey fsismond@coord.iter01.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be "cd /groups/wall2-ilabt-iminds-be/f-interop-coap; tar -zxvf "$PROJ.tar.gz""
+scp -r -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey "$PROJ.tar.gz" fsismond@$HOST_IP:/groups/wall2-ilabt-iminds-be/f-interop-coap/
+ssh -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey fsismond@$HOST_IP "cd /groups/wall2-ilabt-iminds-be/f-interop-coap; tar -zxvf "$PROJ.tar.gz""
