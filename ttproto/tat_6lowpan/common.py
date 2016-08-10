ef369d0fcf44d008e9a0a104cad86a2003ed909e
Tompoariniaina Andriamilanto <tompoariniaina.andriamilanto@irisa.fr>
2016-08-10 15:20:06 +0200

    refact(6lowpan): Fixed a bug into preprocess, removed the get_test_purpose() redefinition and put documentations of 6lowpan into yaml

--- ttproto/tat_6lowpan/common.py
+++ ttproto/tat_6lowpan/common.py
@@ -33,7 +33,8 @@
 
 import re
 
-from ttproto.core.analyzer import TestCase, is_protocol, Node, Conversation, Capture
+from ttproto.core.analyzer import (TestCase, is_protocol, Node, Conversation,
+                                   Capture)
 from ttproto.core.dissector import Frame
 from ttproto.core.templates import All, Not, Any, Length
 from ttproto.core.typecheck import *
@@ -48,42 +49,27 @@ class NoStimuliFoundForTestcase(Error):
     """
     pass
 
+
 class FilterError(Error):
     """
     The error thrown when an error occurs into Filter object during its run
     """
     pass
 
+
 class SixlowpanTestCase(TestCase):
     """
     The test case extension representing a Sixlowpan test case
     """
 
-    @classmethod
     @typecheck
-    def get_test_purpose(cls) -> str:
+    def preprocess(
+        self,
+        capture: Capture
+    ) -> (list_of(Conversation), list_of(Frame)):
         """
-        Get the purpose of this test case
-
-        :return: The purpose of this test case
-        :rtype: str
-        """
-        if cls.__doc__:
-            save = False
-            for line in cls.__doc__.splitlines():
-                if line.startswith('Objective'):
-                    ret = line.split('*')[1]
-                    save = True
-                elif line.startswith('Configuration'):
-                    return ' '.join(ret.split())
-                elif save:
-                    ret += line
-        return ''
-
-    @typecheck
-    def preprocess(self, capture: Capture) -> (list_of(Conversation), list_of(Frame)):
-        """
-        Preprocess and filter the frames of the capture into test case related conversations.
+        Preprocess and filter the frames of the capture into test case related
+        conversations.
 
         :param Capture: The capture which will be filtered/preprocessed
         :return:
@@ -92,7 +78,8 @@ class SixlowpanTestCase(TestCase):
         # TODO assert is subclass of TesCase?
 
         # Get informations from the test case
-        # TODO get attrbutes stimuli , protocol under test, nodes patterns directly from child's atrib?
+        # TODO get attrbutes stimuli , protocol under test, nodes patterns
+        #      directly from child's atrib?
         stimulis = self.get_stimulis()
         print("stimulus:" + str(stimulis))
         protocol = self.get_protocol()
@@ -102,18 +89,23 @@ class SixlowpanTestCase(TestCase):
         conversations = []
         ignored = []
 
-
         # TODO what happens if no protocol declared on the test case?
         if not nodes or len(nodes) < 2:
-            raise ValueError('Expected at leaset two nodes declaration from the test case')
+            raise ValueError(
+                'Expected at leaset two nodes declaration from the test case'
+            )
         if not protocol:
-            raise ValueError('Expected a protocol under test declaration from the test case')
+            raise ValueError(
+                'Expected a protocol under test declaration from the test case'
+            )
         # If there is no stimuli at all
         if not stimulis or len(stimulis) == 0:
-            raise NoStimuliFoundForTestcase('Expected stimuli declaration from the test case')
+            raise NoStimuliFoundForTestcase(
+                'Expected stimuli declaration from the test case'
+            )
 
         # Get the frames filtered on the protocol
-        frames, ignored = Frame.filter_frames(capture, protocol)
+        frames, ignored = Frame.filter_frames(capture.frames, protocol)
 
         # Get a counter of the current stimuli
         sti_count = 0
@@ -167,24 +159,3 @@ class SixlowpanTestCase(TestCase):
             conversations.append(current_conversation)
 
         return conversations, ignored
-
-    # @property
-    # def conversations(self):
-    #     """
-    #     Function to get the conversations as a list
-    #
-    #     :return: The lsit of conversation
-    #     :rtype: [Conversation]
-    #     """
-    #     return self._conversations
-    #
-    # @property
-    # def ignored(self):
-    #     """
-    #     Function to get the ignored frames as a list
-    #
-    #     :return: The ignored frames that were filtered
-    #     :rtype: [Frame]
-    #     """
-    #     return self._ignored
-
--- ttproto/tat_6lowpan/testcases/td_6lowpan_hc_01.py
+++ ttproto/tat_6lowpan/testcases/td_6lowpan_hc_01.py
@@ -1,59 +1,46 @@
-#!/usr/bin/env python3
-
 from ..common import *
 
 
 class TD_6LOWPAN_HC_01 (SixlowpanTestCase):
-
     """
-====================        ===========================================================================================
-Identifier                  TD_6LoWPAN_HC_01
-====================        ===========================================================================================
-Objective                   * Check that EUTs correctly handle compressed 6LoWPAN packets (EUI-64 linklocal, hop limit=64)
-Configuration               * Node - Node
-References                  * RFC 6282 section 3
-Pre-test conditions         * Header compression is enabled on both EUT1 and EUT2
-                            * EUT1 and EUT2 are configured to use EUI-64
-                            * EUT1 and EUT2 are configured with a default hop limit of 64
-====================        ===========================================================================================
-
-=============== =========    ========    ===================================================================================
-Test Sequence   Step         Type        Description
-=============== =========    ========    ===================================================================================
-.. ts           0            Stimulus    * EUT1 initiates an echo request to EUT2's link-local address
-                                         * ICMP payload = 4 bytes, total IPv6 size 52 bytes
-                                         * Hop Limit is 64, no traffic class or flow label is being used
-
-.. ts           1            Check       EUT1 sends a compressed 6LoWPAN packet containing the Echo Request message to EUT2
-
-.. ts           2            Check       Dispatch value in 6LowPAN packet is "011TFxHL"
-
-.. ts           3            Feature     In IP_HC, TF is 11 and the ecn, dscp and flow label fields are compressed away
-
-.. ts           4            Feature     In IP_HC, HLIM (HL) is 10 and the hop limit field is compressed away
-
-.. ts           5            Feature     In IP_HC, SAC=0, SAM=11; DAC=0; DAM=11
-
-.. ts           6            Verify      EUT2 receives the Echo Request message from EUT1
-
-.. ts           7            Check       EUT2 sends a compressed 6LoWPAN packet containing the Echo Reply message to EUT1
-
-.. ts           8            Check       Dispatch value in 6LowPAN packet is "011TFxHL"
-
-.. ts           9            Feature     In IP_HC, TF is 11 and the ecn, dscp and flow label fields are compressed away
-
-.. ts           10           Feature     In IP_HC, HLIM (HL) is 10 and the hop limit field is compressed away
-
-.. ts           11           Feature     In IP_HC, SAC=0, SAM=11; DAC=0; DAM=11
-
-.. ts           12           Verify      EUT1 receives the Echo Reply message from EUT2
-
-=============== =========    ========    ===================================================================================
-
-=============== ============================================================================================================
-Notes           * The feature tests check that best compression is used (but this is not a requirement for interoperability)
-                * The Echo Reply message might use a different hop limit in some implementations, then the HLIM value might also be different.
-=============== ============================================================================================================
+---
+TD_6LOWPAN_HC_01:
+    cfg: Node-Node
+    not:
+        - The feature tests check that best compression is used (but this is
+            not a requirement for interoperability)
+        - The Echo Reply message might use a different hop limit in some
+            implementations, then the HLIM value might also be different.
+    obj: Check that EUTs correctly handle compressed 6LoWPAN packets (EUI-64
+        link-local, hop limit=64)
+    pre:
+        - Header compression is enabled on both EUT1 and EUT2
+        - EUT1 and EUT2 are configured to use EUI-64
+        - EUT1 and EUT2 are configured with a default hop limit of 64
+    ref: RFC 6282 section 3; RFC 6775 5.6
+    seq:
+        -   s:
+            - EUT1 initiates an echo request to EUT2's link-local address
+            - ICMP payload = 4 bytes, total IPv6 size 52 bytes
+            - Hop Limit is 64, no traffic class or flow label is being used
+        -   c: EUT1 sends a compressed 6LoWPAN packet containing the Echo
+                Request message to EUT2
+        -   c: "Dispatch value in 6LowPAN packet is \u201C011TFxHL\u201D"
+        -   f: In IP_HC, TF is 11 and the ecn, dscp and flow label fields are
+                compressed away
+        -   f: In IP_HC, HLIM (HL) is 10 and the hop limit field is compressed
+                away
+        -   f: In IP_HC, SAC=0, SAM=11; DAC=0; DAM=11
+        -   v: EUT2 receives the Echo Request message from EUT1
+        -   c: EUT2 sends a compressed 6LoWPAN packet containing the Echo Reply
+                message to EUT1
+        -   c: "Dispatch value in 6LowPAN packet is \u201C011TFxHL\u201D"
+        -   f: In IP_HC, TF is 11 and the ecn, dscp and flow label fields are
+                compressed away
+        -   f: In IP_HC, HLIM (HL) is 10 and the hop limit field is compressed
+                away
+        -   f: In IP_HC, SAC=0, SAM=11; DAC=0; DAM=11
+        -   v: EUT1 receives the Echo Reply message from EUT2
     """
 
     @classmethod
