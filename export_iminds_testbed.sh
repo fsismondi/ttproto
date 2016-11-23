46914f2ad4bf63a07f5945e6dd9cc2c1e1108473
Federico Sismondi <federicosismondi@gmail.com>
2016-11-23 15:08:03 +0100

    removed unused files and code

--- export_iminds_testbed.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-PROJ=$(basename `git rev-parse --show-toplevel`)
-HOST_IP=193.190.127.249
-git archive --format=tar --prefix="$PROJ/" master | gzip >"$PROJ.tar.gz"
-scp -r -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey "$PROJ.tar.gz" fsismond@$HOST_IP:/groups/wall2-ilabt-iminds-be/f-interop-coap/
-ssh -i /Users/fsismondi/F-Interop/20160500_fed4fire-CoAP-remote-PoC/iMindsTestBed.privateKey fsismond@$HOST_IP "cd /groups/wall2-ilabt-iminds-be/f-interop-coap; tar -zxvf "$PROJ.tar.gz""
