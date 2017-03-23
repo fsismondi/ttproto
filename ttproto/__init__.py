dca3e3269d818c00f0c3faaff55c68c167ff0fa9
Federico Sismondi <federicosismondi@gmail.com>
2017-03-23 13:43:26 +0100

    fix for /tmp dir creation

--- ttproto/__init__.py
+++ ttproto/__init__.py
@@ -1,3 +1,16 @@
 # -*- coding:utf-8 -*-
+import os, errno
 
 __version__ = '0.1.1'  
+
+# Directories
+DATADIR = "data"
+TMPDIR = "tmp"
+LOGDIR = "log"
+
+for d in TMPDIR, DATADIR, LOGDIR:
+    try:
+        os.makedirs(d)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
