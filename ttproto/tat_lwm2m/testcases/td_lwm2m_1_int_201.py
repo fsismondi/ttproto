be4bea2e50b95bd0650afed2591e6dadbe3c4e2a
Mahdi Ben Alaya <benalaya@sensinov.com>
2018-04-27 19:09:33 +0200

    update LWM2M and IPSO specification

--- ttproto/tat_lwm2m/testcases/td_lwm2m_1_int_201.py
+++ ttproto/tat_lwm2m/testcases/td_lwm2m_1_int_201.py
@@ -4,49 +4,106 @@ from ..common import *
 class TD_LWM2M_1_INT_201(CoAPTestCase):
     """
 ---
-TD_LWM2M_1_INT_201:
-    testcase_id: TD_LWM2M_1_INT_201
-uri : http://openmobilealliance.org/iot/lightweight-m2m-lwm2m
-configuration: LWM2M_CFG_01
-objective: 
-  - Quering the following data on the client (Device Object = ID 3) in plain text format
-  - - Manufacturer
-    - Model number
-    - Serial number
-pre_conditions: Device is registred at the LWM2M server
-notes: null
-references: 'OMA-ETS-LightweightM2M-V1_0-20160829-C'
-    seq:
-    - step_id: 'TD_LWM2M_1_INT_201_step_01'
-    type: stimuli
-    node : coap_server
+testcase_id: TD_LWM2M_1.0_INT_201 
+uri : http://openmobilealliance.org/iot/lightweight-m2m-lwm2m 
+configuration: LWM2M_CFG_01 
+objective:   
+  - Querying the following data on the client (Device Object = ID 3) in text plain format 
+  - - Manufacturer 
+    - Model number 
+    - Serial number 
+pre_conditions: 
+  - Device is registered at the LWM2M server
+  - Client is supporting Plain Text data format (0)
+  - The Client supports the minimum Configuration C.1.
+references: 'OMA-ETS-LightweightM2M-V1_0_1-20170926-A'
+sequence: 
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_01'
+    type: stimuli 
+    node : lwm2m_server 
+    description: 
+      - 'Server sends a READ request (COAP GET) on device object’s Manufacturer resource'       
+      - - Type = 0 (CON)         
+        - Code = 1 (GET) 
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_02' 
+    type: check 
     description:
-      - 'Server sends a READ request (COAP GET) on device object resources'
-      - - Type = 0 (CON)
+      - 'The request sent by the server contains'   
+      - - Type=0 and Code=1  
+          - Accept option = text/plain
+          - Uri-Path option = 3/0/0
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_03'
+    type: check
+    description:       
+      - 'Client sends response containing'         
+      - - Code = 2.05 (Content)           
+        - Non-empty Payload 
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_04' 
+    type: verify 
+    node: lwm2m_server 
+    description:         
+      - 'Requested data is successfully displayed' 
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_05' 
+    type: stimuli 
+    node : lwm2m_server 
+    description: 
+      - 'Server sends a READ request (COAP GET) on device object’s Model Number resource'       
+      - - Type = 0 (CON)         
+        - Code = 1 (GET) 
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_06' 
+    type: check 
+    description:       
+      - 'The request sent by the server contains'       
+      - - Type=0 and Code=1         
+        - Accept option = text/plain                 
+        - Uri-Path option = 3/0/1 
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_07' 
+    type: check 
+    description:         
+      - 'Client sends response containing'         
+      - - Code = 2.05 (Content)
+        - Non-empty Payload
+ 
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_08' 
+    type: verify 
+    node: lwm2m_server 
+    description:         
+      - 'Requested data is successfully displayed' 
+
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_09' 
+    type: stimuli 
+    node : lwm2m_server 
+    description: 
+      - 'Server sends a READ request (COAP GET) on device object’s Serial Number resource'
+      - - Type = 0 (CON)         
         - Code = 1 (GET)
+ 
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_10' 
+    type: check 
+    description:       
+      - 'The request sent by the server contains'       
+      - - Type=0 and Code=1         
+        - Accept option = text/plain                 
+        - Uri-Path option = 3/0/2
+ 
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_11' 
+    type: check 
+    description:         
+      - 'Client sends response containing'         
+      - - Code = 2.05 (Content)           
+        - Non-empty Payload
 
-  - step_id: 'TD_LWM2M_1_INT_201_step_02'
-    type: check
-    description:
-      - 'The request sent by the server contains'
-      - - Type=0 and Code=1
-        - Accept option = text/plain
-
-
-  - step_id: 'TD_LWM2M_1_INT_201_step_03'
-    type: check
-    description:
-        - 'Client sends response containing'
-        - - Code = 2.05 (Content)
-          - Manufacturer
-          - Model number
-          - Serial number
-
-  - step_id: 'TD_LWM2M_1_INT_201_step_04'
-    type: verify
-    node: coap_server
-    description:
-        - 'Requested data is successfully displayed'
+  - step_id: 'TD_LWM2M_1.0_INT_201_step_12' 
+    type: verify 
+    node: lwm2m_server 
+    description:         
+      - 'Requested data is successfully displayed'
     """
 
     @classmethod
@@ -59,15 +116,36 @@ references: 'OMA-ETS-LightweightM2M-V1_0-20160829-C'
         :return: The stimulis of this TC
         :rtype: [Value]
         """
-        return [CoAP(code='get')]
+        return [CoAP(type='con', code='get')]
 
     def run(self):
-        self.match('server', CoAP(type='con', code='get', opt=self.uri('/3/0')))
-        self.match('server', CoAP(opt=Opt(CoAPOptionAccept())), 'fail')
-        CMID = self.coap['mid']
-        CTOK = self.coap['tok']
+        self.match('server', CoAP(type='con', code='get', opt=self.uri('/3/0/0')), 'fail')
+        self.match('server', CoAP(pl=(b'')), 'fail')
+        self.match('server', CoAP(opt=Opt(CoAPOptionAccept('40'))), 'fail')
+        
+        self.next()
+
+        self.match('client', CoAP(code=2.05, pl=Not(b'')), 'fail')
+        self.match('client', CoAP(opt=Opt(CoAPOptionContentFormat('40'))), 'fail')
 
         self.next()
 
-        if self.match('client', CoAP(code=2.05, mid=CMID, tok=CTOK, pl=Not(b'')), None):
-            self.match('client', CoAP(opt=Opt(CoAPOptionContentFormat())), 'fail')
+        self.match('server', CoAP(type='con', code='get', opt=self.uri('/3/0/1')), 'fail')
+        self.match('server', CoAP(pl=(b'')), 'fail')
+        self.match('server', CoAP(opt=Opt(CoAPOptionAccept('40'))), 'fail')
+        
+        self.next()
+
+        self.match('client', CoAP(code=2.05, pl=Not(b'')), 'fail')
+        self.match('client', CoAP(opt=Opt(CoAPOptionContentFormat('40'))), 'fail')
+        
+        self.next()
+
+        self.match('server', CoAP(type='con', code='get', opt=self.uri('/3/0/2')), 'fail')
+        self.match('server', CoAP(pl=(b'')), 'fail')
+        self.match('server', CoAP(opt=Opt(CoAPOptionAccept('40'))), 'fail')
+        
+        self.next()
+
+        self.match('client', CoAP(code=2.05, pl=Not(b'')), 'fail')
+        self.match('client', CoAP(opt=Opt(CoAPOptionContentFormat('40'))), 'fail')
--- ttproto/tat_lwm2m/testcases/td_lwm2m_1_int_203.py
+++ ttproto/tat_lwm2m/testcases/td_lwm2m_1_int_203.py
@@ -4,49 +4,53 @@ from ..common import *
 class TD_LWM2M_1_INT_203(CoAPTestCase):
     """
 ---
-TD_LWM2M_1_INT_203:
-    testcase_id: TD_LWM2M_1_INT_203
+testcase_id: TD_LWM2M_1.0_INT_203
 uri : http://openmobilealliance.org/iot/lightweight-m2m-lwm2m
 configuration: LWM2M_CFG_01
 objective: 
-  - Quering the following data on the client (Device Object = ID 3) in tlvl format
-  - - Manufacturer
-    - Model number
-    - Serial number
-pre_conditions: Device is registred at the LWM2M server
-notes: null
-references: 'OMA-ETS-LightweightM2M-V1_0-20160829-C'
-    seq:
-     - step_id: 'TD_LWM2M_1INT_203_step_01'
+  - Quering the Resources values of Device Object (ID:3) on the Client in TLV format
+  - - Manufacturer Name (id:0)
+    - Model number (ID:1)
+    - Serial number (ID:2)
+    - Firware Version (ID:3)
+    - Error Code (ID:11)
+    - Supported Binding and Modes (ID:16)
+pre_conditions:
+  - Device is registred at the LWM2M server
+  - The Client supports the minimum Configuration C.1.
+references: 'OMA-ETS-LightweightM2M-V1_0_1-20170926-A'
+sequence:
+  - step_id: 'TD_LWM2M_1.0_INT_203_step_01'
     type: stimuli
-    node : coap_server
+    node : lwm2m_server
     description:
-      - 'Server sends a READ request (COAP GET) on device object resources'
+      - 'Server sends a READ request (COAP GET) on device object instance'
       - - Type = 0 (CON)
         - Code = 1 (GET)
 
-  - step_id: 'TD_LWM2M_1_INT_203_step_02'
+  - step_id: 'TD_LWM2M_1.0_INT_203_step_02'
     type: check
     description:
       - 'The request sent by the server contains'
       - - Type=0 and Code=1
         - Accept option = application/vnd.oma.lwm2m+tlv
+        - Uri-Path option = 3/0
 
 
-  - step_id: 'TD_LWM2M_1_INT_203_step_03'
+  - step_id: 'TD_LWM2M_1.0_INT_203_step_03'
     type: check
     description:
         - 'Client sends response containing'
         - - Code = 2.05 (Content)
-          - Manufacturer
-          - Model number
-          - Serial number
+          - content-format option=application/vnd.oma.lwm2m+tlv
+          - Non-empty payload= contains the expected values of Manufacturer Name, Model number, Serial number, Firmware Version, error code, and supported Binding and modes resources. 
 
-  - step_id: 'TD_LWM2M__INT_203_step_04'
+  - step_id: 'TD_LWM2M_1.0_INT_203_step_04'
     type: verify
-    node: coap_server
+    node: lwm2m_server
     description:
         - 'Requested data is successfully displayed'
+
     """
 
     @classmethod
@@ -59,15 +63,14 @@ references: 'OMA-ETS-LightweightM2M-V1_0-20160829-C'
         :return: The stimulis of this TC
         :rtype: [Value]
         """
-        return [CoAP(code='get')]
+        return [CoAP(type='con', code='get')]
 
     def run(self):
-        self.match('server', CoAP(type='con', code='get', opt=self.uri('/3/0')))
-        self.match('server', CoAP(opt=Opt(CoAPOptionAccept())), 'fail')
-        CMID = self.coap['mid']
-        CTOK = self.coap['tok']
-
+        self.match('server', CoAP(type='con', code='get', opt=self.uri('/3/0')), 'fail')
+        self.match('server', CoAP(pl=(b'')), 'fail')
+        self.match('server', CoAP(opt=Opt(CoAPOptionAccept('11542'))), 'fail')
+        
         self.next()
 
-        if self.match('client', CoAP(code=2.05, mid=CMID, tok=CTOK, pl=Not(b'')), None):
-            self.match('client', CoAP(opt=Opt(CoAPOptionContentFormat())), 'fail')
+        self.match('client', CoAP(code=2.05, pl=Not(b'')), 'fail')
+        self.match('client', CoAP(opt=Opt(CoAPOptionContentFormat('11542'))), 'fail')
