1ca6d87bf768709606a79e41595b97b936166239
ggharbi <gharbi.ghada@gmail.com>
2018-11-02 15:08:56 +0100

    fixes of match options

--- ttproto/tat_onem2m/testcases/td_m2m_nh_24.py
+++ ttproto/tat_onem2m/testcases/td_m2m_nh_24.py
@@ -64,7 +64,7 @@ TD_M2M_NH_24:
         
             self.next()
 
-            self.match("server", CoAP(type='con', code=Any(65, 68), pl=Not(b'')), 'fail')
+            self.match("server", CoAP(code=Any(65, 68), pl=Not(b'')), 'fail')
             self.match('server', CoAP(opt=Opt(CoAPOptionContentFormat())), 'fail')
             self.match('server', CoAP(opt=Opt(CoAPOptionOneM2MResponseStatusCode('2004'))), 'fail')
             self.match('server', CoAP(opt=Opt(CoAPOptionOneM2MRequestIdentifier(RIVAL))), 'fail')
